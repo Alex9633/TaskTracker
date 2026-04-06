e718f8902565086c3910e9387aa7676660dd478a
Alex9633 <alex.milanovmc@gmail.com>
2026-04-06 17:40:30 +0200

    Add files

---
+++ TaskTracker/TaskItem.cs
@@ -0,0 +1,7 @@
+﻿public class TaskItem
+{
+    public string Title { get; set; } = "";
+    public string StartDate { get; set; } = "";
+    public string DueDate { get; set; } = "";
+    public bool IsCompleted { get; set; }
+}
